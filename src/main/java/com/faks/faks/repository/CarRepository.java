b32a38cc43e844da2120a1193e2e6a8f3e9e73ef
ficipo <filipsikiric@gmail.com>
2021-04-06 12:00:33 +0200

    First Commit

--- src/main/java/com/faks/faks/repository/CarRepository.java
+++ src/main/java/com/faks/faks/repository/CarRepository.java
@@ -5,10 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface CarRepository extends JpaRepository<Car, Long>, JpaSpecificationExecutor<Car> {
 
-    List<Car> getByName(String name);
+
 }
